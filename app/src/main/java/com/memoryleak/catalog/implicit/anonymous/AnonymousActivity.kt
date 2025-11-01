4f0dbd0d94733579343ad783af8b43d6a2baf4aa
SallySalem <eng.sallysalem@gmail.com>
2025-11-01 22:35:37 +0100

    Add anonymous class ml fix

--- app/src/main/java/com/memoryleak/catalog/implicit/anonymous/AnonymousActivity.kt
+++ app/src/main/java/com/memoryleak/catalog/implicit/anonymous/AnonymousActivity.kt
@@ -3,7 +3,35 @@ package com.memoryleak.catalog.implicit.anonymous
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.memoryleak.catalog.R
+import java.lang.ref.WeakReference
 
+class AnonymousActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anonymous)
+
+        anonymousObject = WeakReference(object : MyAnonymousClass() {
+            override fun doSomething() {
+                this@AnonymousActivity
+            }
+        })
+    }
+
+    companion object {
+        private lateinit var anonymousObject: WeakReference<MyAnonymousClass>
+    }
+}
+
+open class MyAnonymousClass {
+    open fun doSomething() {
+    }
+}
+
+
+/*
+* Memory leak in anonymous class
+*
+*
 class AnonymousActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,3 +53,4 @@ open class MyAnonymousClass {
     open fun doSomething() {
     }
 }
+ */
