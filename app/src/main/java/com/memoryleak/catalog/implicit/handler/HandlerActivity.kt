983006573a65071e7e93ae37ab4cddd6a67af9f2
SallySalem <eng.sallysalem@gmail.com>
2025-11-16 19:44:37 +0100

    Fix handler memory leak by use sttic inner class and weakReference

--- app/src/main/java/com/memoryleak/catalog/implicit/handler/HandlerActivity.kt
+++ app/src/main/java/com/memoryleak/catalog/implicit/handler/HandlerActivity.kt
@@ -9,31 +9,54 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.memoryleak.catalog.R
+import java.lang.ref.WeakReference
 
 class HandlerActivity : AppCompatActivity() {
 
 //    private var innerHandler: Handler = Handler(Looper.getMainLooper())
-     private val innerHandler: Handler = Handler(Looper.getMainLooper()) { msg: Message ->
-        println(this@HandlerActivity)
-        true
+//     private val innerHandler: Handler = Handler(Looper.getMainLooper()) { msg: Message ->
+//        println(this@HandlerActivity)
+//        true
+//    }
+
+    private class MyHandler(val activity: WeakReference<HandlerActivity>): Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            println(activity.get())
+        }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_handler)
 
-        //Scheduling Runnable
+        /*
+         * Scheduling Runnable example for leak and fix
+         */
+
 //        innerHandler.postDelayed({
 //            println(this@HandlerActivity)
 //        }, 50000)
 
+        //Fix memory leak using static inner class and use weakReference for the object used by handler
+//        val activity = WeakReference(this)
+//        MyHandler(activity).postDelayed({
+//            println(activity.get())
+//        }, 50000)
 
+
+
+        /*
+         * Scheduling Message example for leak and fix
+         */
         //Scheduling Message
-        val message: Message = innerHandler.obtainMessage(1)
-        innerHandler.sendMessageDelayed(message, 50000)
-    }
+//        val message: Message = innerHandler.obtainMessage(1)
+//        innerHandler.sendMessageDelayed(message, 50000)
 
-    override fun onDestroy() {
-        super.onDestroy()
-        innerHandler.removeCallbacksAndMessages(null)
+        //Fix memory leak using static inner class and use weakReference for the object used by handler
+        val activity = WeakReference(this)
+        val myHandler = MyHandler(activity)
+        val message: Message = myHandler.obtainMessage(1)
+        myHandler.sendMessageDelayed(message, 50000)
     }
 }
